e489a0a46f68ebc962eed7196f789d7abe69fa26
juhtam <juhasztomi123@gmail.com>
2025-09-07 10:48:01 +0200

    hazi kesz

--- pro.py
+++ pro.py
@@ -29,6 +29,22 @@ print(tuple([1,2,3]))
 
 #HF: Git parancsok
 # git init: adott mappa verziókövetésének indítása
-# git add . :
+# git add . : minden változást hozzáad.
+# git clone [repo link]: meglévő rpo letöltése.
+# git status: repo aktuális állapota
+# git commit -m "üzenet" stagingben lévő változások mentése commitba üzenettel
+# git branch : branchek listázása
+# git branch [név]: új branch létrehozás
+# git checkout [branch]: átváltás másik branchere
+# git remote -v: megnézi, hogy milyen távoli repok vannak beállítva, például Github link
+# git push origin [branch]: aktuális branch feltoltése távoli repoba
+# git pull: frissítések hozzáadása távoli repoból.
+# git config --global user.name "név": megadjuk a github nevünket
+# git config --global user.email "email": megadjuk a github email címünket.
+# git remote add origin [link]: hozzáad egy távoli repót
+# git branch -M main: átnevezi az aktuális branchet main-re
+# git restore [fájlnév]: utolsó commitra állítja vissza
+# git config --list: összes beállítást megmutatja
+
 
 
