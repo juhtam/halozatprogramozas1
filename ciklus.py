7373de0a1c665c2893ac5028943a3193e9f65a7d
juhtam <juhasztomi123@gmail.com>
2025-09-12 12:32:22 +0200

    szoroztabla

--- ciklus.py
+++ ciklus.py
@@ -21,3 +21,36 @@ def paroselemek(paroselem):
 # print(f"a lista elemeinek összege: {osszegez(lista)}")
 
 print(f"a lista páros elemeinek darabszáma: {paroselemek(lista)}")
+
+
+#írass ki egy sorba 5 *-ot
+print("*"*5)
+
+for i in range(5):
+    print("*", end="")
+print()
+
+#rajzolj piramist.
+#       *
+#      ***
+#     *****
+#    *******
+#   *********
+#  ***********
+# *************
+
+
+n = 5
+space_db = n - 1
+csillag_db = 1
+for i in range(n):
+    print(" " * space_db, end = "")
+    print("*" * csillag_db)
+    space_db -= 1
+    csillag_db += 2
+
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i*j:10}", end = " ")
+    print()
