97626bdd747beacda2d5ca9175aa3527e1b46f8a
juhtam <juhasztomi123@gmail.com>
2025-09-12 12:57:46 +0200

    osszeg

---
+++ ciklus2.py
@@ -0,0 +1,19 @@
+# kérjünk be számokat és adjuk meg az összegükat.
+
+# while bennmaradási feltétel:
+#     ciklus mag
+bekertszam = None
+bekertszamok = []
+while bekertszam != 0:
+    bekertszam = int(input("Kérem a számot: "))
+    bekertszamok.append(bekertszam)
+    
+
+print(f"A {bekertszamok} összege egyenlő {sum(bekertszamok)}")
+
+osszeg = 0
+be_n = None
+while be_n != 0:
+    be_n = int(input("kérek egy számot: "))
+    osszeg += be_n
+print(osszeg)
